99c6731502136dce864ec51bce16948d60cb6ecf
Patryk <patryk_mej@o2.pl>
2020-11-28 16:08:46 +0100

    Templates and python file

--- guess_number_flask.py
+++ guess_number_flask.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/', methods=['POST', 'GET'])
+def guess_number():
+    return render_template('index.html')
+
+
+app.run()
